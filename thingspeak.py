7ba6219375e7ba425278783e9efab3a0f5513183
Steve Gibson <Steve.Gibson@spectrumhealth>
2015-08-19 23:42:44 -0400

    Adjusted fields for Pi Camp

--- thingspeak.py
+++ thingspeak.py
@@ -1,5 +1,5 @@
 import httplib, urllib
-params = urllib.urlencode({'field1':89, 'field2':"TheGiblet", 'key':'T0U6G8PQVFTIDWJI'})
+params = urllib.urlencode({'field1':"Steve", 'field2':"None", 'key':'T0U6G8PQVFTIDWJI'})
 headers = {"Content-type":"application/x-www-form-urlencoded", "Accept": "text/plain"}
 conn = httplib.HTTPConnection("api.thingspeak.com:80")
 conn.request("POST", "/update", params, headers)
